efc5ce780e70b91f5ded580549aa3b3c9723adc1
Krivenko Vassiliy <VKrivenko@beeline.kz>
2023-05-22 23:54:36 +0600

    add API request JS code

--- script.js
+++ script.js
@@ -14,3 +14,50 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000);
+
+let pokemonElement1 = document.getElementById("pokemon1");
+let pokemonElement2 = document.getElementById("pokemon2");
+let pokemonElement3 = document.getElementById("pokemon3");
+
+let randomPokemon1 = Math.floor(Math.random() * 500) + 1;
+let randomPokemon2 = Math.floor(Math.random() * 500) + 1;
+let randomPokemon3 = Math.floor(Math.random() * 500) + 1;
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon1}`)
+    .then(resp => resp.json())
+    .then(data => {
+        let name = data.name;
+        let image = data.sprites.front_default;
+
+        pokemonElement1.innerHTML = `
+        <h2 class="pokemon-name">${name}</h2>
+        <img class="pokemon-img" src="${image}" alt="${name}">
+        `;
+    })
+    .catch(error => console.error(error));
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon2}`)
+    .then(resp => resp.json())
+    .then(data => {
+        let name = data.name;
+        let image = data.sprites.front_default;
+
+        pokemonElement2.innerHTML = `
+        <h2 class="pokemon-name">${name}</h2>
+        <img class="pokemon-img" src="${image}" alt="${name}">
+        `;
+    })
+    .catch(error => console.error(error));
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon3}`)
+    .then(resp => resp.json())
+    .then(data => {
+        let name = data.name;
+        let image = data.sprites.front_default;
+
+        pokemonElement3.innerHTML = `
+        <h2 class="pokemon-name">${name}</h2>
+        <img class="pokemon-img" src="${image}" alt="${name}">
+        `;
+    })
+    .catch(error => console.error(error));
